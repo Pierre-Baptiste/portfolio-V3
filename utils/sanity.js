6f5fbfb4ad86e51e097b5e52326f79f97809c62b
Pierre-Baptiste Dupire <pierre-baptiste@omnidoc.fr>
2020-11-15 14:44:00 +0100

    sanity

--- utils/sanity.js
+++ utils/sanity.js
@@ -2,8 +2,10 @@ import {
   createClient,
   createImageUrlBuilder,
   createPortableTextComponent,
-  createPreviewSubscriptionHook
-} from 'next-sanity'
+  createPreviewSubscriptionHook,
+} from "next-sanity";
+
+const BlockContent = require("@sanity/block-content-to-react");
 
 const config = {
   /**
@@ -15,49 +17,117 @@ const config = {
    **/
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  useCdn: process.env.NODE_ENV === 'production'
+  useCdn: process.env.NODE_ENV === "production",
   /**
    * Set useCdn to `false` if your application require the freshest possible
    * data always (potentially slightly slower and a bit more expensive).
    * Authenticated request (like preview) will always bypass the CDN
    **/
-}
+};
 
 if (!config.projectId) {
-  throw Error('The Project ID is not set. Check your environment variables.')
+  throw Error("The Project ID is not set. Check your environment variables.");
 }
 if (!config.dataset) {
-  throw Error('The dataset name is not set. Check your environment variables.')
+  throw Error("The dataset name is not set. Check your environment variables.");
 }
 
 /**
  * Set up a helper function for generating Image URLs with only the asset reference data in your documents.
  * Read more: https://www.sanity.io/docs/image-url
  **/
-export const urlFor = source => createImageUrlBuilder(config).image(source)
+export const urlFor = (source) => createImageUrlBuilder(config).image(source);
 
 // Set up the live preview subsscription hook
-export const usePreviewSubscription = createPreviewSubscriptionHook(config)
+export const usePreviewSubscription = createPreviewSubscriptionHook(config);
 
-// Set up Portable Text serialization
-export const PortableText = createPortableTextComponent({
-  ...config,
-  // Serializers passed to @sanity/block-content-to-react
-  // (https://github.com/sanity-io/block-content-to-react)
-  serializers: {}
-})
+const BlockRenderer = (props) => {
+  const { style = "p" } = props.node;
+
+  if (/^h\d/.test(style)) {
+    const level = style.replace(/[^\d]/g, "");
+    return React.createElement(
+      style,
+      {
+        className: `heading-${level} text-2xl font-bold mb-8 md:mt-16 mt-12 uppercase font-sans text-black`,
+      },
+      props.children
+    );
+  }
+
+  if (style === "blockquote") {
+    return <blockquote>- {props.children}</blockquote>;
+  }
+
+  if (style === "p") {
+    return React.createElement(
+      style,
+      {
+        className: `my-4`,
+      },
+      props.children
+    );
+  }
+
+  if (style === "normal") {
+    return React.createElement(
+      style,
+      {
+        className: `my-4`,
+      },
+      props.children
+    );
+  }
+
+  // Fall back to default handling
+  return BlockContent.defaultSerializers.types.block(props);
+};
+
+const highlight = (props) => {
+  return (
+    <span className="text-5xl leading-5 font-serif font-bold text-black">
+      {props.children}
+    </span>
+  );
+};
+
+export const createBlockContent = (blocks) => {
+  return (
+    <BlockContent
+      blocks={blocks}
+      {...config}
+      serializers={{
+        container: (props) => {
+          return (
+            <div className="text-sm leading-5 text-gray-700 pt-8">
+              {props.children}
+            </div>
+          );
+        },
+        types: {
+          code: (props) => (
+            <pre data-language={props.node.language}>
+              <code>{props.node.code}</code>
+            </pre>
+          ),
+          block: BlockRenderer,
+        },
+        list: (props) => <ul className="list-disc pl-12">{props.children}</ul>,
+        marks: { highlight },
+      }}
+    />
+  );
+};
 
 // Set up the client for fetching data in the getProps page functions
-export const sanityClient = createClient(config)
+export const sanityClient = createClient(config);
 // Set up a preview client with serverless authentication for drafts
-
-
 
 export const previewClient = createClient({
   ...config,
   useCdn: false,
-})
+});
 
 // Helper function for easily switching between normal client and preview client
-export const getClient = usePreview =>
-  usePreview ? previewClient : sanityClient
+export const getClient = (usePreview) =>
+  usePreview ? previewClient : sanityClient;
