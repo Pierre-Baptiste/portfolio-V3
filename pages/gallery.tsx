099a60503e399725f61c43b0da9f689f5d2dc969
Pierre-Baptiste Dupire <pierre-baptiste@omnidoc.fr>
2020-11-18 22:09:50 +0100

    uselayout warning

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import Head from "next/head";
 import { Layout } from "components/core/Layout";
@@ -52,6 +52,13 @@ const shuffleArr = (array) => {
 export default function gallery({ photos }) {
   // so the order of the pictures is randomized
   photos = shuffleArr(photos);
+  const [showChild, setShowChild] = useState(false);
+
+  // Wait until after client-side hydration to show
+  useEffect(() => {
+    setShowChild(true);
+  }, []);
+
   return (
     <>
       <Head>
@@ -70,28 +77,30 @@ export default function gallery({ photos }) {
                 thumbnail: false,
               }}
             >
-              <Gallery
-                photos={photos}
-                renderImage={({ photo, index }) => (
-                  <LightgalleryItem
-                    group="gallery"
-                    src={photo.src}
-                    itemClassName="-my-1"
-                    subHtml={`<a href=${photos[index].captionLink}><h4>${photos[index].caption}</h4></a>`}
-                    key={photo.src}
-                  >
-                    <Image
+              {showChild && (
+                <Gallery
+                  photos={photos}
+                  renderImage={({ photo, index }) => (
+                    <LightgalleryItem
+                      group="gallery"
                       src={photo.src}
-                      width={photo.width}
-                      height={photo.height}
+                      itemClassName="-my-1"
+                      subHtml={`<a href=${photos[index].captionLink}><h4>${photos[index].caption}</h4></a>`}
                       key={photo.src}
-                      data-index={index}
-                      className="hover:scale-110 cursor-pointer transition-transform duration-200 transform overflow-hidden"
-                      alt={photos[index].alt}
-                    />
-                  </LightgalleryItem>
-                )}
-              />
+                    >
+                      <Image
+                        src={photo.src}
+                        width={photo.width}
+                        height={photo.height}
+                        key={photo.src}
+                        data-index={index}
+                        className="hover:scale-110 cursor-pointer transition-transform duration-200 transform overflow-hidden"
+                        alt={photos[index].alt}
+                      />
+                    </LightgalleryItem>
+                  )}
+                />
+              )}
             </LightgalleryProvider>
           </div>
         </Layout>
